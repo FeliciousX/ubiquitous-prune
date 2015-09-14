9f7c9da22c7e153884619c3b4bd1e22f81b4aa8c
feliciousx <feliciousx@gmail.com>
2015-09-14 14:48:39 +0800

    Implements interative way of reversing linked list

--- data-structures/linked_list.cpp
+++ data-structures/linked_list.cpp
@@ -34,18 +34,19 @@ int main() {
 }
 
 Node* Reverse(Node *head) {
-    cout << "reversing: " << head->data << endl;
-    Spit(head);
-    if (head->next == NULL) {
-        return head;
+    Node* current = head;
+    Node* prev = NULL;
+    Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
 
-    Node *temp = head;
-    head = Reverse(temp->next);
-    temp->next = NULL;
-    head->next = temp;
-    Spit(head);
-    return temp;
+    head = prev;
+    return head;
 }
 
 void ReversePrint(Node *head) {
